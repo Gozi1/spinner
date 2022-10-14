2b7e0f92b91180c2439bec355c1334c1fb6d4027
Gozi1 <snwabiani@gmail.com>
2022-10-14 12:58:49 +0000

    add spinner2

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+
+const spinnerAnimations = ['\r|   ','\r/   ','\r-   ','\r\\   ']
+const spinnerAnimation = function(animations,delay,loops,initialDelay){
+  if(loops >= 0){
+    let index = (loops % animations.length); 
+    setTimeout(() => { process.stdout.write(animations[index])}, initialDelay);
+    spinnerAnimation(animations,delay,loops-1,initialDelay+delay);
+  }
+}
+spinnerAnimation(spinnerAnimations,200,8,100);
